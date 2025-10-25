3af2bb41d0b7b34aaf54e1bccf8aa78b59850f58
kouhei.takashima <kouhei.takashima@kortvaluta.biz>
2025-10-25 15:54:11 +0900

    fix: syntax error

--- backend/app/controllers/api/admin_controller.rb
+++ backend/app/controllers/api/admin_controller.rb
@@ -1,31 +1,30 @@
-module Api do 
+module Api
     class AdminController < ActionController::Base
-
-        def create_event 
+        def create_event
             begin
                 created_event = Event.create!(
                     name: params[:name],
                     date: params[:date],
                     info: params[:info]
                 )
-                render 'shared/http_status', locals: { code: '201', message:
-                    HttpStatusHelper::ERROR_CODE['message']['201'] }, status: :created
+                render "shared/http_status", locals: { code: "201", message:
+                    HttpStatusHelper::ERROR_CODE["message"]["201"] }, status: :created
             rescue Exception
-                render 'shared/http_status', locals: { code: '500', message:
-                    HttpStatusHelper::ERROR_CODE['message']['500'] }, status: :error
+                render "shared/http_status", locals: { code: "500", message:
+                    HttpStatusHelper::ERROR_CODE["message"]["500"] }, status: :error
             end
         end
 
         def verify_event_code
             event_user = Event_Users.find_by(invitation_code: params[:invitation_code])
             if event_user.nil?
-                render 'shared/http_status', locals: { code: '404', message:
-                    HttpStatusHelper::ERROR_CODE['message']['404'] }, status: :not_found
+                render "shared/http_status", locals: { code: "404", message:
+                    HttpStatusHelper::ERROR_CODE["message"]["404"] }, status: :not_found
             else
                 user = User.find_by(id: event_user.user_id)
                 data = {
-                    :is_ok => true,
-                    :user_name => user.name
+                    is_ok: true,
+                    user_name: user.name
                 }
                 render json: data.to_json()
             end
